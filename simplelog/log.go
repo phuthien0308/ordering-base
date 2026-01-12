0e1772418576ba42150a54ffba28f9af47124a62
Thien Ly <thienly@Thiens-MacBook-Pro.local>
2026-01-11 22:27:38 -0500

    initial

---
+++ simplelog/log.go
@@ -0,0 +1,55 @@
+package simplelog
+
+import (
+	"context"
+
+	"github.com/phuthien0308/ordering-base/simplelog/tags"
+	"go.uber.org/zap"
+)
+
+type SimpleLogKey struct{}
+
+var SimpleLogKeyCtx = SimpleLogKey{}
+
+type SimpleZapLogger struct {
+	*zap.Logger
+}
+
+func NewSimpleZapLogger(logger *zap.Logger) *SimpleZapLogger {
+	return &SimpleZapLogger{logger}
+}
+
+func (logger *SimpleZapLogger) withContext(ctx context.Context) *SimpleZapLogger {
+	if fields, ok := ctx.Value(SimpleLogKeyCtx).([]zap.Field); ok {
+		return &SimpleZapLogger{logger.With(fields...)}
+	}
+	return logger
+}
+
+func (logger *SimpleZapLogger) Debug(ctx context.Context, msg string, fields ...tags.T) {
+	logger.withContext(ctx).Logger.Debug(msg, fields...)
+}
+
+func (logger *SimpleZapLogger) Info(ctx context.Context, msg string, fields ...tags.T) {
+	logger.withContext(ctx).Logger.Info(msg, fields...)
+}
+
+func (logger *SimpleZapLogger) Warn(ctx context.Context, msg string, fields ...tags.T) {
+	logger.withContext(ctx).Logger.Warn(msg, fields...)
+}
+
+func (logger *SimpleZapLogger) Error(ctx context.Context, msg string, fields ...tags.T) {
+	logger.withContext(ctx).Logger.Error(msg, fields...)
+}
+
+func (logger *SimpleZapLogger) DPanic(ctx context.Context, msg string, fields ...tags.T) {
+	logger.withContext(ctx).Logger.DPanic(msg, fields...)
+}
+
+func (logger *SimpleZapLogger) Panic(ctx context.Context, msg string, fields ...tags.T) {
+	logger.withContext(ctx).Logger.Panic(msg, fields...)
+}
+
+func (logger *SimpleZapLogger) Fatal(ctx context.Context, msg string, fields ...tags.T) {
+	logger.withContext(ctx).Logger.Fatal(msg, fields...)
+}
---
+++ simplelog/log_test.go
@@ -0,0 +1,28 @@
+package simplelog
+
+import (
+	"context"
+	"testing"
+
+	"github.com/phuthien0308/ordering-base/simplelog/tags"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zaptest/observer"
+)
+
+func TestLogger(t *testing.T) {
+	zapCore, observerLogs := observer.New(zap.InfoLevel)
+	zapLogger := zap.New(zapCore)
+	logger := NewSimpleZapLogger(zapLogger)
+	ctx := context.WithValue(context.TODO(), SimpleLogKeyCtx, []tags.T{tags.String("trace_id", "1")})
+	logger.Info(ctx, "hello world")
+	result := false
+	for _, entry := range observerLogs.All() {
+		if _, ok := entry.ContextMap()["trace_id"]; ok {
+			result = true
+		}
+	}
+	if !result {
+		t.Fail()
+	}
+
+}
